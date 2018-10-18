21c866f6d019333ff4fe9e376804e5851dfaaca5
ABoyHasNooName <39313183+ABoyHasNooName@users.noreply.github.com>
2018-10-18 18:09:35 +0530

    Update config.sample.php

--- config.sample.php
+++ config.sample.php
@@ -1,16 +1,16 @@
 <?php
   // Login to my.telegram.org
-  $APP_ID = "YOUR_APP_ID_HERE";
-  $API_HASH = "YOUR_API_HASH_HERE";
+  $APP_ID = "338883";
+  $API_HASH = "d3c72e1bf0ceda9c0935698ead948ce9";
   // Get this from @BotFather
-  $BOT_TOKEN = "YOUR_TOKEN_HERE";
+  $BOT_TOKEN = "621227770:AAEDRTnMW27Akw3naQ7XrqHDRhXvVB08Fqw";
   // FileName for the @MadeLineProto session
   $BOT_SESSION = "YAUploadBot.session";
   // location to store the downloaded files
   $TMP_DOWNLOADS = __DIR__ . '/temporary_downloads';
   // array to store who can use the bot
   $OWNER_IDS = array(
-    "TELEGRAM ID 1",
-    "TELEGRAM ID 2",
-    "TELEGRAM ID 3"
+    "7351948",
+    "496970480",
+    "528272188"
   );
